b6b9bf962b394ef6a631648415d120d6daa13b62
onepiecehy <411388213@qq.com>
2017-04-13 22:38:50 +0800

    coding...

--- Datastruct_AlgInC/Charpter5/hashSep.c
+++ Datastruct_AlgInC/Charpter5/hashSep.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+
+//define variable
+#define TABLESIZE 1000
+typedef struct{
+    char *key = NULL;
+    char *value = NULL;
+    Node *next = NULL;
+}Node;
+
+Node *table[TABLESIZE];
+
+//define hash function
+
+//define conflict handle function
+
